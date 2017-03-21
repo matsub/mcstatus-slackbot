1db1d8a304b4bd3e17e69ec47b1e603af453a0d9
matsub <matsub.rk@gmail.com>
2017-03-22 01:39:05 +0900

    modify server

--- run.py
+++ run.py
@@ -2,39 +2,65 @@
 # -*- coding: utf-8 -*-
 
 import configparser
-from mcinfo.server import Server
-from mcinfo.utils import McRespond
+from slashcommands import SlashCommands
+from mcinfo.utils import (
+    address_match,
+    McRespond,
+)
 
 config = configparser.ConfigParser()
 config.read('configuration.ini')
 TOKEN = config['Slack']['TOKEN']
 
-app = Server(TOKEN, prefix='/mcinfo/')
+app = SlashCommands(TOKEN, prefix='/mcinfo/')
 mc = McRespond()
 
 
+def read_address(request):
+    if 'text' in request:
+        return address_match(request['text'])
+    else:
+        return None
+
+
 @app.route('/online')
-def online_list(address):
+def online_list(body):
+    address = read_address(body)
     players = mc.status(address).players
-    return "{0.online} / {0.max} players online".format(players)
+
+    response = {
+        "response_type": "in_channel",
+        "text": "{0.online} / {0.max} players online".format(players),
+    }
+    return response
 
 
 @app.route('/list')
-def player_list(address):
-    response = mc.query(address)
-    if response is None:
-        return """\
+def player_list(body):
+    address = read_address(body)
+    query = mc.query(address)
+
+    response = {"response_type": "in_channel"}
+    if query is None:
+        response['text'] = """\
         I couldn't get player list (This server doesn't support query.).
         """
     else:
-        return """\
+        response['text'] = """\
         The server has following players online: {0}
-        """.format(", ".join(response.players.names))
+        """.format(", ".join(query.players.names))
+    return response
 
 
 @app.route('/version')
-def server_version(address):
-    return mc.status(address).version.name
+def server_version(body):
+    address = read_address(body)
+
+    response = {
+        "response_type": "in_channel",
+        "text": mc.status(address).version.name,
+    }
+    return response
 
 
 if __name__ == '__main__':
